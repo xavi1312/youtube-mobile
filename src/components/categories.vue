3add790267566167eb3209ad8f970af9b56d7f8f
Xavi <xavierisachalemany@gmail.com>
2020-05-25 14:09:07 +0200

    translate categories text

--- src/components/categories.vue
+++ src/components/categories.vue
@@ -31,37 +31,37 @@
       return {
         categories: [
           {
-            name: "Tendencias",
+            name: "Trending",
             background:
               "https://cdn.pixabay.com/photo/2016/05/01/21/20/earth-1365995_960_720.jpg",
             icon: "mdi-fire",
           },
           {
-            name: "Música",
+            name: "Music",
             background:
               "https://cdn.pixabay.com/photo/2015/03/26/09/58/headphones-690685_960_720.jpg",
             icon: "mdi-music",
           },
           {
-            name: "Videojuagos",
+            name: "Gaming",
             background:
               "https://cdn.pixabay.com/photo/2017/08/10/04/54/playstation-2618338_960_720.jpg",
             icon: "mdi-youtube-gaming",
           },
           {
-            name: "Noticias",
+            name: "News",
             background:
               "https://cdn.pixabay.com/photo/2012/12/20/10/12/article-71342_960_720.jpg",
             icon: "mdi-newspaper",
           },
           {
-            name: "Películas",
+            name: "Movies",
             background:
               "https://cdn.pixabay.com/photo/2019/05/19/10/40/cinema-4213751_960_720.jpg",
             icon: "mdi-filmstrip-box",
           },
           {
-            name: "Aprendizaje",
+            name: "Learning",
             background:
               "https://cdn.pixabay.com/photo/2016/06/29/08/50/pencil-1486278_960_720.jpg",
             icon: "mdi-lightbulb",
